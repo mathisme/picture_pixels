06be9ecc3f9469d958c7a8c44d3fecadbb3663b2
mathisme <annedl1211@live.com>
2022-03-30 16:56:29 -0500

    started the js, just need to fill in the functions

--- pixels.js
+++ pixels.js
@@ -1,1 +1,13 @@
-console.log("testing");
+view = {
+	dimension_text_element = document.getElementById("dim"),
+	corners_text_element = docoment.getElementById("corners"),
+	submit_button = document.getElementById("submit"),
+	output_paragraph = document.getElementById("output")
+};
+
+controller = {
+	init : function(){},
+	button_clicked : function(){}
+}
+
+controller.init()
