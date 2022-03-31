9590907a6eb9eae796a30fed113d0ae38c906f92
Mathisme <annedl1211@live.com>
2022-03-31 09:44:38 -0500

    created file for unit testing

---
+++ testPicture.py
@@ -0,0 +1,21 @@
+from picture import Picture
+import unittest
+
+class TestPicture(unittest.TestCase):
+    def test_init(self):
+        pass
+         
+    def test_getminmax(self):
+        pass
+    
+    def test_get_pixels(self):
+        pass
+        
+    def test_valid_dim(self):
+        pass
+       
+    def test_valid_corners(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
